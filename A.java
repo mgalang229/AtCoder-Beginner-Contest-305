9e072beab97735bae04622455e6d7fc8cd976ae8
jasonpogi <51401355+mgalang229@users.noreply.github.com>
2023-06-10 21:47:57 +0800

    Update A.java

--- A.java
+++ A.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.StringTokenizer;
 
-public class Main {
+public class A {
 	
 	public static void main(String[] args) {
 		FastScanner fs = new FastScanner();
